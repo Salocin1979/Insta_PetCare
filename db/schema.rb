2ff3c68531eda3a9e6e68aafc9fbb26fb772ed74
Tsiky Razafindrabe <rova.nytsiky@gmail.com>
2021-11-11 21:30:11 +0400

    add photo uploaded from cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_154400) do
+ActiveRecord::Schema.define(version: 2021_11_11_165924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_11_11_154400) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "specie_id", null: false
+    t.string "photo"
     t.index ["specie_id"], name: "index_animals_on_specie_id"
     t.index ["user_id"], name: "index_animals_on_user_id"
   end
