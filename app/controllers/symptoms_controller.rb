5d0d8f31538e145728736fced9adbffe61b0bea7
frederic dupuche <dupuchefrederic@gmail.com>
2021-11-25 18:52:10 +0400

    merge symptom

--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -1,16 +1,8 @@
 class SymptomsController < ApplicationController
-  def index
-      @symptoms = Symptom.all
-    end
+
 
     def show
-      @symptoms =Symptom.find(symptom_params[:specie_id])
+      @symptoms =Symptom.find(params[:id])
     end
 
-    private
-
-    def symptom_params
-      params.require(:symptom).permit(:suggestion, :description, :specie_id)
-    end
-    
 end
