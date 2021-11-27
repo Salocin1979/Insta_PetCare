aa7c1c129b67b91f0695ada08c869c0dc434e0a7
Nicolas Bagnath <nicolas.bagnath@gmail.com>
2021-11-27 19:02:04 +0400

    Add Photo to the seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   # validates :first_name, presence: true
   # validates :last_name, presence: true
-
+  has_one_attached :photo
   has_many :animals, dependent: :destroy
   has_many :consultations, dependent: :destroy
 end
